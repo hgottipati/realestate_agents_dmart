12280d5c5247bf513c152a1e170b474c74d2afc8
Hareesh Gottipati <hareesh.x.gottipati.-nd@disney.com>
2017-12-07 22:43:26 -0800

    add all tables

--- sql/V1__ddls.sql
+++ sql/V1__ddls.sql
@@ -1,38 +1,26 @@
 IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'agents')
 create table  agents
 (	agentid int IDENTITY(1,1) primary key,
-	agentfname varchar(40) not null,
-	agentlname varchar(40) not null
+	agentfirstname varchar(40) not null,
+	agentlastname varchar(40) not null
 )
 
 IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'customers')
 create table customers
 (
 	custid int IDENTITY(1,1) primary key,
-	custfname varchar(40) not null,
-	custlname varchar(40) not null,
-	custphone varchar(50) not null,
+	custfirstname varchar(40) not null,
+	custlastname varchar(40) not null,
+	custphone varchar(50)  null,
 	custemailid varchar(60) not null,
-	custjoindt datetime default getdate()
+	custjoindate datetime default getdate()
 )
 
 IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'tours')
 create table tours
 (	
 	tourid int IDENTITY(1,1) primary key,
-	tourdt datetime default getdate()
-)
-
-IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'agentdeals')
-create table agentdeals
-(
-	agentid int primary key,
-	custid int ,
-	tourid int,
-	propertyid int,
-	listid int,
-	purchaseid int,
-	
+	tourdate datetime default getdate()
 )
 
 IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'properties')
@@ -40,32 +28,61 @@ create table properties
 (
 	propertyid int identity (1,1) PRIMARY KEY,
 	propertyNo int,
-	type varchar (20),
+	style varchar (20),
 	address1 varchar (20),
 	address2 varchar (20),
 	city varchar (30),
 	postcode varchar(10),
 	state char(2),
 	country varchar(40)
-
 )
 
 IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'listings')
 create table listings
 (
 	listid int identity (1,1) primary key,
-	listdt datetime2 ,
+	listdate datetime2
+)
+
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'buying')
+create table buying
+(
+	purchaseid int identity (1,1) primary key,
+	purchasedate datetime2
+)
+
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'selling')
+create table selling
+(
+	saleid int identity (1,1) primary key,
+	saledate datetime2
+)
+
+
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES  WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'agentdeals')
+create table agentdeals
+(
+	agentid int primary key,
+	custid int ,
+	tourid int,
+	listid int,
+	propertyid int,
+	purchaseid int,
+	saleid int,
+	purchaseprice int,
 	listprice int,
-	delistdt datetime2
+	saleprice int
 )
 
 
 ALTER TABLE agentdeals ADD FOREIGN KEY (agentid )		REFERENCES  agents		(agentid)
 ALTER TABLE agentdeals ADD FOREIGN KEY (custid )		REFERENCES  customers	(custid)
 ALTER TABLE agentdeals ADD FOREIGN KEY (tourid )		REFERENCES  tours		(tourid)
+ALTER TABLE agentdeals ADD FOREIGN KEY (listid )		REFERENCES  listings    (listid)
 ALTER TABLE agentdeals ADD FOREIGN KEY (propertyid )	REFERENCES  properties	(propertyid)
--- ALTER TABLE agentdeals ADD FOREIGN KEY (listid )		REFERENCES  listings    (listid)
--- ALTER TABLE agentdeals ADD FOREIGN KEY (purchaseid )    REFERENCES  sales		(purchaseid)
+ALTER TABLE agentdeals ADD FOREIGN KEY (purchaseid )    REFERENCES  buying		(purchaseid)
+ALTER TABLE agentdeals ADD FOREIGN KEY (saleid )		REFERENCES  selling	  	(saleid)
+
 
 
 
